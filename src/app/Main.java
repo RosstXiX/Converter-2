fd8d6d4c009b7830e85a5c2cf59d2b092d6f2932
Rostislav <rstk.tischenko@gmail.com>
2025-02-06 19:44:55 +0200

    Add kilometers to miles convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,11 +7,17 @@ public class Main {
         System.out.println("Converter app");
 
         int mil = 2;
-        System.out.printf("%d miles equals %.2f kilometers", mil, milToKm(mil));
+        System.out.printf("%d miles equals %.2f kilometers\n", mil, milToKm(mil));
+        int km = 5;
+        System.out.printf("%d kilometers equals %.2f miles\n", km, kmToMil(km));
 
     }
 
     private static double milToKm(double mil) {
         return mil / RATIO;
+    }
+
+    private static double kmToMil(double km) {
+        return km * RATIO;
     }
 }
